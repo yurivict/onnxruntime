d79173ec1a1cfe7ebc45314669762c0f922c29c4
stevenlix <stevli@microsoft.com>
2019-10-02 20:18:33 -0700

    pass arguments to script

--- tools/ci_build/github/linux/docker/scripts/install_ubuntu.sh
+++ tools/ci_build/github/linux/docker/scripts/install_ubuntu.sh
@@ -144,12 +144,12 @@ else
         /usr/bin/python${PYTHON_VER} -m pip install --upgrade --force-reinstall pip==19.0.3
     fi
 
-    /usr/bin/python3 -m pip install --upgrade --force-reinstall numpy==1.15.0
-    /usr/bin/python3 -m pip install --upgrade --force-reinstall requests==2.21.0
+    /usr/bin/python${PYTHON_VER} -m pip install --upgrade --force-reinstall numpy==1.15.0
+    /usr/bin/python${PYTHON_VER} -m pip install --upgrade --force-reinstall requests==2.21.0
     if [ $DEVICE_TYPE = "Normal" ]; then
-        /usr/bin/python3 -m pip install --upgrade --force-reinstall sympy==1.1.1
+        /usr/bin/python${PYTHON_VER} -m pip install --upgrade --force-reinstall sympy==1.1.1
     fi
-    /usr/bin/python3 -m pip install --upgrade scipy
+    /usr/bin/python${PYTHON_VER} -m pip install --upgrade scipy
     rm -rf /var/lib/apt/lists/*
 
     if [ $DEVICE_TYPE = "Normal" ]; then
