6eb890cb9aa5b262fa2711c7fbc82ab15f696c2d
Changming Sun <chasun@microsoft.com>
2019-10-25 01:02:35 -0700

    a

--- onnxruntime/core/platform/windows/debug_alloc.h
+++ onnxruntime/core/platform/windows/debug_alloc.h
@@ -2,16 +2,4 @@
 // Licensed under the MIT License.
 
 #pragma once
-#ifndef NDEBUG
-// TVM need to run with shared CRT, so won't work with debug heap alloc
-#if !(defined USE_TVM || (defined USE_NGRAPH && defined _WIN32))
-void* DebugHeapAlloc(size_t size, unsigned framesToSkip = 0);
-void* DebugHeapReAlloc(void* p, size_t size);
-void DebugHeapFree(void* p) noexcept;
 
-#define calloc CallocNotImplemented
-#define malloc DebugHeapAlloc
-#define realloc DebugHeapReAlloc
-#define free DebugHeapFree
-#endif
-#endif
